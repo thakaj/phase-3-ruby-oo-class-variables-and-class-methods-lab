0de0ba38dd6e4f8bd8e860c8bbfaa7392d1c37b4
thakaj <taulant.hakaj@scranton.edu>
2021-08-03 16:24:51 -0400

    commit tree ca7b84ce18511820e344c38da51438156a386b8d

---
+++ lib/song.rb
@@ -0,0 +1,15 @@
+class Song
+    attr_accessor :name, :artist, :genre
+    @@count = 0
+    @@artists = []
+    @@genres = []
+    def initialize(name, artist, genre)
+        @name = name
+        @artist = artist
+        @genre = genre
+        @@count += 1
+        @@genres << genre
+        @@artists << artist
+    end
+    
+end
